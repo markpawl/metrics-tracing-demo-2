e0476405febdfc2a6f5c92eca619440c90a60444
Mark Pawlowski <markpaw@gmail.com>
2019-06-26 15:32:02 -0400

    initial commit

---
+++ src/main/java/com/example/metricstracingdemo2/MetricsTracingDemo2Application.java
@@ -0,0 +1,13 @@
+package com.example.metricstracingdemo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetricsTracingDemo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetricsTracingDemo2Application.class, args);
+	}
+
+}
